6f04b45c0d021fb6abadab9299afc87a33777f3c
Muhammad Shaharyar <88059515+M-Shaharyar@users.noreply.github.com>
2024-07-20 20:44:33 +0500

    Time: 179 ms (23.18%) | Memory: 18 MB (56.92%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,17 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        my_map =  dict()
-        max_num = float('-inf')
-        num = 0
-        for i in nums:
-            if i not in my_map:
-                my_map[i]=1
-                if my_map[i] > max_num:
-                    max_num = my_map[i]
-                    num = i
-            elif i in my_map:
-                my_map[i]+=1
-                if my_map[i] > max_num:
-                    max_num = my_map[i]
-                    num = i
-        return num
+        count = {}
+        res,maxCount = 0,0
+        for n in nums:
+            count[n] = 1 + count.get(n,0)
+            res = n if count[n]>maxCount else res
+            maxCount = max(count[n],maxCount)
+        return res
