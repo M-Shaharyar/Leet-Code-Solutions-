0f81067a4a9cd35f640ff2060f121298e1b7f2e2
Muhammad Shaharyar <88059515+M-Shaharyar@users.noreply.github.com>
2024-07-22 10:47:40 +0500

    Time: 103 ms (31.44%) | Memory: 17.9 MB (49.46%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,10 +1,30 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        count = len(nums)//3 +1
+        size = len(nums)//3+1
         ls = []
-        my_map = {}
+        count1 , count2 , element1 , element2 = 0 , 0 , 0 , 0
         for i in nums:
-            my_map[i] = 1 + my_map.get(i,0)
-            if my_map[i] == count:
-                ls.append(i)
+            if count1 == 0 and i != element2:
+                count1 = 1
+                element1 = i
+            elif count2==0 and i != element1:
+                count2 = 1
+                element2 = i
+            elif i == element1:
+                count1+=1
+            elif i == element2:
+                count2+=1
+            else:
+                count1-=1
+                count2-=1
+        count1,count2 = 0,0
+        for i in nums:
+            if i == element1:
+                count1+=1
+            elif i == element2:
+                count2+=1
+        if count1>=size:
+            ls.append(element1)
+        if count2>=size:
+            ls.append(element2)
         return ls
