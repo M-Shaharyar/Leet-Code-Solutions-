fcc0c476ad490c5de173bfe5e8d093235b83967a
Muhammad Shaharyar <88059515+M-Shaharyar@users.noreply.github.com>
2024-08-13 21:20:06 +0500

    Time: 47 ms (93.45%) | Memory: 16.9 MB (10.62%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.py
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -2,9 +2,8 @@ class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
         ans = []
         i = 0
-        while i < len(nums)//2:
+        for i in range(n):
             ans.append(nums[i])
             ans.append(nums[i+n])
-            i += 1
         return ans
         
