1b9e36905635939e149326edb1fe9db7bd5f790a
Muhammad Shaharyar <88059515+M-Shaharyar@users.noreply.github.com>
2024-08-28 09:33:34 +0500

    Time: 61 ms (8.75%) | Memory: 16.5 MB (50.89%) - LeetSync

--- 3429-special-array-i/special-array-i.py
+++ 3429-special-array-i/special-array-i.py
@@ -1,9 +1,6 @@
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
-        if len(nums) == 1:
-            return True
-        elif len(nums) == 0:
-            return False
+        
         for i in range(len(nums)-1):
             if nums[i] % 2 == 0 and nums[i+1] % 2 == 0:
                 return False
