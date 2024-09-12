7867d570bbcf38802429d4b45da6781159998d3d
Muhammad Shaharyar <88059515+M-Shaharyar@users.noreply.github.com>
2024-09-12 12:32:32 +0500

    Time: 155 ms (69.45%) | Memory: 28.1 MB (27.82%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,12 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        res = [0]*len(nums)
-        for i in range(len(nums)):
-            if i+k < len(nums):
-                res[i+k] = nums[i]
-            elif i + k > len(nums)-1:
-                res[(i+k)%len(nums)] = nums[i]
-        for i in range(len(res)):
-            nums[i] = res[i]
+        k = k % len(nums)
+
+        l , r = 0 , len(nums) - 1
+        while l < r:
+            nums[l] , nums[r] = nums[r] , nums[l]
+            l , r = l + 1, r-1
+        
+        l,r = 0 , k - 1
+        while l < r:
+            nums[l],nums[r] = nums[r], nums[l]
+            l , r = l + 1 , r - 1
+        
+        l,r = k , len(nums) - 1
+        while l < r:
+            nums[l] , nums[r] = nums[r], nums[l]
+            l,r = l + 1 , r - 1
         
