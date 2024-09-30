fcf276ebafeeec7c27b23e8db7f31ba9a88614ad
Muhammad Shaharyar <88059515+M-Shaharyar@users.noreply.github.com>
2024-09-30 06:46:27 +0500

    Time: 411 ms (83.05%) | Memory: 35.7 MB (16.44%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,7 +1,16 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums.sort()
-        for i,n in enumerate(nums):
-            if i+1 < len(nums) and n == nums[i+1]:
+        # nums.sort()
+        # for i,n in enumerate(nums):
+        #     if i+1 < len(nums) and n == nums[i+1]:
+        #         return True
+        # return False
+        hash_table = {}
+        for i in range(len(nums)):
+            if nums[i] not in hash_table:
+                hash_table[nums[i]] = i
+            else:
                 return True
         return False
+
+
