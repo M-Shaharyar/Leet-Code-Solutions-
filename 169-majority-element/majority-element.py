aeddb3cf0381f0d35084a1480e61cad2f2fb4ffd
Muhammad Shaharyar <88059515+M-Shaharyar@users.noreply.github.com>
2024-10-03 06:25:42 +0500

    Time: 178 ms (25.77%) | Memory: 18.1 MB (27.68%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,5 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        size = len(nums)
-        return nums[size//2] 
+        count = {}
+        res , maxCount = 0 , 0 
+        for n in nums:
+            count[n] = 1 + count.get(n,0)
+            res = n if count[n] > maxCount else res
+            maxCount = max(count[n],maxCount)
+        return res
