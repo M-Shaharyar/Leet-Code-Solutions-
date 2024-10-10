45e934dcd8ece80a7b549ac343079eb543b46b58
Muhammad Shaharyar <88059515+M-Shaharyar@users.noreply.github.com>
2024-10-10 06:53:59 +0500

    Time: 99 ms (48.51%) | Memory: 16.6 MB (44.51%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -7,7 +7,7 @@ class Solution:
         for R in range(len(s)):
             count[s[R]] = 1 + count.get(s[R],0)
 
-            while (R - L + 1) - max(count.values()) > k:
+            if (R - L + 1) - max(count.values()) > k:
                 count[s[L]] -= 1
                 L += 1
             
