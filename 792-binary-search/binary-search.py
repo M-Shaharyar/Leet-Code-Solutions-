5c7d3a8500cb1a392bf8779f9303106efacfd716
Muhammad Shaharyar <88059515+M-Shaharyar@users.noreply.github.com>
2024-10-14 07:42:03 +0500

    Time: 209 ms (5.46%) | Memory: 18.1 MB (75.34%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,7 +1,9 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
+
         for i in range(len(nums)):
-            if nums[i] == target:
-                return i
+            for j in range(len(nums)):
+                if nums[j] == target:
+                    return j
         return -1
+            
