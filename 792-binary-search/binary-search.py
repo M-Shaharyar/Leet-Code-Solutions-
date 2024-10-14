53f3e52cf285367cf0fcb84cf9c86d0f69e5fd51
Muhammad Shaharyar <88059515+M-Shaharyar@users.noreply.github.com>
2024-10-14 07:44:02 +0500

    Time: 199 ms (15.24%) | Memory: 18.2 MB (7.68%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -3,7 +3,8 @@ class Solution:
 
         for i in range(len(nums)):
             for j in range(len(nums)):
-                if nums[j] == target:
-                    return j
+                for k in range(len(nums)):
+                    if nums[k] == target:
+                        return k
         return -1
             
