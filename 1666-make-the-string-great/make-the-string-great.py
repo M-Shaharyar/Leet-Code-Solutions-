28560792113e13fda4e52d40ef1dad3ab311b515
Muhammad Shaharyar <88059515+M-Shaharyar@users.noreply.github.com>
2024-10-23 07:55:16 +0500

    Time: 4 ms (95.84%) | Memory: 16.6 MB (10.17%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.py
+++ 1666-make-the-string-great/make-the-string-great.py
@@ -1,35 +1,16 @@
 class Solution:
-    def makeGood(self,s:str)->str:
-        def lower(c):
-            if ord(c) < ord('a'):
-                return chr(ord('a') + ord(c) - ord('A')) 
-            return c
-        
-        stack = []
+    def makeGood(self, s: str) -> str:
+        stack  = []
         i = 0
         while i < len(s):
-            if (stack and 
-                stack[-1] != s[i] and 
-                lower(stack[-1]) == lower(s[i])):
+            if (
+                stack and 
+                stack[-1] != s[i] and
+                stack[-1].lower() == s[i].lower()):
                 stack.pop()
-            else:
+            else: 
                 stack.append(s[i])
             i +=1
+        return "".join(stack)
+            
 
-        return "".join(stack) 
-        
-        
-        
-        
-        
-        # strr = ""
-        # i = 0
-        # while i < len(s) - 1:
-        #     if s[i] != s[i+1] and s[i].lower() == s[i+1].lower():
-        #         i += 2 
-        #         continue
-        #     strr += s[i]
-        #     i += 1
-        # if i < len(s):
-        #     strr += s[i]
-        # return strr
